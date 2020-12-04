5ad9661b15e1783d2740c99fc8db43e284ac00d0
Mike Manchee <mikemanchee@gmail.com>
2020-12-04 08:57:48 -0800

    add reducer to first test = passed

--- src/__test__/reducers/cargo-list-reduser.test.js
+++ src/__test__/reducers/cargo-list-reduser.test.js
@@ -1,7 +1,9 @@
 import cargoListReducer from '../../reducers/cargo-list-reducer';
 
 describe('cargoListReducer', () => {
+  
   test('Should return state if no action is passed to the reducer', () => {
     expect(cargoListReducer({}, { type: null })).toEqual({});
   });
+
 });
