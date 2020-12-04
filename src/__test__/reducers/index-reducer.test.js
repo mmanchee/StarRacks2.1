d098d9251e90fed55acba8a84bd5661b9c13f614
Mike Manchee <mikemanchee@gmail.com>
2020-12-04 14:50:22 -0800

    debug a test, all tests passing

--- src/__test__/reducers/index-reducer.test.js
+++ src/__test__/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import cargoListReducer from '../../reducers/cargo-list-reducer';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
+import * as c from './../../actions/ActionTypes';
 
 let store = createStore(rootReducer);
 
@@ -19,4 +20,23 @@ describe("rootReducer", () => {
   test('Check initial state of formVisible matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
+  test('Check that ADD_CARGO action work with cargoListReducer and root reducer', () => {
+    const action = {
+      type: c.ADD_CARGO,
+      Name: 'Monster Dog (aka Jadashha)',
+      Description: 'Jadashha from Q\'onoS',
+      Crates: 8,
+      EnergyCredits: 20,
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().cargoManifest).toEqual(cargoListReducer(undefined, action));
+  });
+  test('Check that TOGGLE_FORM action work with formVisibleReducer and root reducer', () => {
+    const action = {
+      type: c.TOGGLE_FORM
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  })
 });
