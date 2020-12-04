da4bca41661944f957d9c55b87489ce3e654bf63
Mike Manchee <mikemanchee@gmail.com>
2020-12-04 09:16:52 -0800

    add reducer to second test - passed

--- src/reducers/cargo-list-reducer.js
+++ src/reducers/cargo-list-reducer.js
@@ -1,6 +1,18 @@
 export default (state = {}, action) => {
-  
+  const { Name, Description, Crates, EnergyCredits, id } = action;
+  switch (action.type) {
+  case 'ADD_CARGO':
+    return Object.assign({}, state, {
+      [id]: {
+        Name: Name,
+        Description: Description,
+        Crates: Crates,
+        EnergyCredits: EnergyCredits,
+        id: id
+      }
+    });
+  default:
     return state;
- 
+  }
 };
 
