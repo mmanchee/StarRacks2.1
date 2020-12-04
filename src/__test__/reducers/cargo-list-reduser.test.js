6d6a0e0536f3b233c157447a8acff8c856333f87
Mike Manchee <mikemanchee@gmail.com>
2020-12-04 09:15:55 -0800

    add second test- failed

--- src/__test__/reducers/cargo-list-reduser.test.js
+++ src/__test__/reducers/cargo-list-reduser.test.js
@@ -1,9 +1,39 @@
 import cargoListReducer from '../../reducers/cargo-list-reducer';
 
 describe('cargoListReducer', () => {
-  
+
+  let action;
+  const cargoData = {
+    Name: 'Cats',
+    Description: 'Orange Cat from Zanthi',
+    Crates: 8,
+    EnergyCredits: 20,
+    id: 1
+  };
+
   test('Should return state if no action is passed to the reducer', () => {
     expect(cargoListReducer({}, { type: null })).toEqual({});
   });
 
+  test('Should accurately add new cargo to the cargoManifest', () => {
+    const { Name, Description, Crates, EnergyCredits, id } = cargoData;
+    action = {
+      type: 'ADD_CARGO',
+      Name: Name,
+      Description: Description,
+      Crates: Crates,
+      EnergyCredits: EnergyCredits,
+      id: id
+    };
+
+    expect(cargoListReducer({}, action)).toEqual({
+      [id]: {
+        Name: Name,
+        Description: Description,
+        Crates: Crates,
+        EnergyCredits: EnergyCredits,
+        id: id
+      }
+    })
+  })
 });
--- src/reducers/cargo-list-reducer.js
+++ src/reducers/cargo-list-reducer.js
@@ -1,3 +1,6 @@
 export default (state = {}, action) => {
-  return state;
+  
+    return state;
+ 
 };
+
