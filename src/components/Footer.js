089883f34e604388f78bdffddf521d8d5fb457d5
Mike Manchee <mikemanchee@gmail.com>
2020-12-10 13:58:44 -0800

    add links to git hub

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <React.Fragment>
       <p>This site is only an example project for <a href="https://www.epicodus.com/">Epicodus</a></p>
+      <p>See the GitHub link <a href="https://github.com/mmanchee/StarRacks2.1">Repo</a></p>
     </React.Fragment>
   );
 }
